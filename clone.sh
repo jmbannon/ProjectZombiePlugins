1778f1b438e2516880ed9eb2369d99214556235a
Jesse Bannon <jmbannon@uw.edu>
2016-11-12 12:01:56 -0800

    Pom and submodule

--- clone.sh
+++ clone.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-git clone https://github.com/jmbannon/LootDrops.git
-git clone https://github.com/jmbannon/CrackshotEnhanced.git
-git clone https://github.com/jmbannon/CompassNavigation.git
-git clone https://github.com/jmbannon/ConsistentChatApi.git
-git clone https://github.com/jmbannon/CustomInteractions.git
-git clone https://github.com/jmbannon/SurvivalTeams.git
-git clone https://github.com/jmbannon/DynamicRegions.git
-git clone https://github.com/jmbannon/BlockBuffer.git
-git clone https://github.com/jmbannon/RegionRotation.git 
 mkdir -p dependencies
 wget -O ./dependencies/Crackshot.jar https://dev.bukkit.org/media/files/885/752/CrackShot.jar
