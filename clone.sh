4f33f947da2aff304c97b7b7ac5dc651da6ada75
Jesse Bannon <jmbannon@uw.edu>
2016-11-12 12:01:56 -0800

    # This is a combination of 3 commits. # The first commit's message is: Pom and submodule WIP
    # This is the 2nd commit message:
    
    submodule
    
    # This is the 3rd commit message:
    
    submodules

--- clone.sh
+++ clone.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 git clone https://github.com/jmbannon/LootDrops.git
 git clone https://github.com/jmbannon/CrackshotEnhanced.git
 git clone https://github.com/jmbannon/CompassNavigation.git
@@ -9,3 +8,5 @@ git clone https://github.com/jmbannon/SurvivalTeams.git
 git clone https://github.com/jmbannon/DynamicRegions.git
 git clone https://github.com/jmbannon/BlockBuffer.git
 git clone https://github.com/jmbannon/RegionRotation.git 
+mkdir -p dependencies
+wget -O ./dependencies/Crackshot.jar https://dev.bukkit.org/media/files/885/752/CrackShot.jar
